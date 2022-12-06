1a742a9652a95c1cafb6d1e4dc60d6404f97db76
Vlad Surubariu <vladsurubariu@gmail.com>
2022-12-06 21:45:18 +0200

    Add HSV camera and color recognition for blue

--- ColorDetection/main.py
+++ ColorDetection/main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 from matplotlib import pyplot as plt
 
@@ -6,11 +7,18 @@ cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 1200)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 800)
 
+lower_blue = np.array([90,50,50])
+upper_blue = np.array([130,255,255])
+
+lower_green = np.array([43, 132, 122])
+upper_green = np.array([63, 250, 250])
+
 def take_photo():
     ret, frame = cap.read();
     
     cv.imwrite('webcamphoto.jpg', frame)
     cap.release()
+    
     
 def take_video():
     
@@ -29,21 +37,23 @@ def take_video():
     
     cap.release()
     cv.destroyAllWindows()
-
+    
 
 def detect_color():
     while True:
         ret, frame = cap.read()
-        height, width, _ = frame.shape()
+        height, width, x = frame.shape
         
         cx = int(width/2)
-        cy = int(width/2)
+        cy = int(height/2)
         
-        pixel_center = frame(cx, cy)
-        cv.circle(frame, (cx, cy), 5, (255, 0, 0), 3)
+        hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+        
+        pixel_center = frame[cx, cy]
+        cv.circle(hsv, (cx, cy), 5, (255, 0, 0), 3)
         
         
-        cv.imshow('Frame', frame)
+        cv.imshow('Frame', hsv)
     
         if cv.waitKey(1) == ord('q'):
             break
@@ -52,5 +62,35 @@ def detect_color():
     cv.destroyAllWindows()
         
         
+def video_hsv_center_circle():
+    while True:
+        ret, frame = cap.read()
+        height, width, x = frame.shape
         
-detect_color()
+        cx = int(width/2)
+        cy = int(height/2)
+        
+        pixel_center = frame[cx, cy]
+        hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+        mask = cv.inRange(hsv, lower_green, upper_green)
+        
+        result = cv.bitwise_and(frame, frame, mask = mask)
+        cv.circle(result, (cx, cy), 5, (255, 0, 0), 3)
+        
+        cv.imshow('Frame', result)
+    
+        if cv.waitKey(1) == ord('q'):
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+       
+       
+def find_color_code():
+    BGR_color = np.array([[[5, 250, 29]]], dtype=np.uint8)
+    x = cv.cvtColor(BGR_color, cv.COLOR_RGB2HSV)
+
+    print(x[0][0])
+
+if __name__ == "__main__":
+    video_hsv_center_circle()
