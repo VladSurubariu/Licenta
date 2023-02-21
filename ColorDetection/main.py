9163fa168ca6f88d47827e78c19fe7ba08a92bd3
Vlad Surubariu <vladsurubariu@gmail.com>
2023-02-21 23:13:09 +0200

    Multiple Color Detection - red and blue

--- ColorDetection/main.py
+++ ColorDetection/main.py
@@ -7,14 +7,17 @@ cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 1200)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 800)
 
-lower_blue = np.array([90,50,50])
-upper_blue = np.array([130,255,255])
+lower_blue = np.array([90,50,50], np.uint8)
+upper_blue = np.array([130,255,255], np.uint8)
 
-lower_green = np.array([43, 132, 122])
-upper_green = np.array([63, 250, 250])
+lower_green = np.array([43, 132, 122], np.uint8)
+upper_green = np.array([63, 250, 250], np.uint8)
+
+lower_red = np.array([136,87,111], np.uint8)
+upper_red = np.array([180,255,255], np.uint8)
 
 def take_photo():
-    ret, frame = cap.read();
+    ret, frame = cap.read()
     
     cv.imwrite('webcamphoto.jpg', frame)
     cap.release()
@@ -84,6 +87,55 @@ def video_hsv_center_circle():
     
     cap.release()
     cv.destroyAllWindows()
+    
+    
+def detectMultipleColors():
+    while(True):
+        _, img = cap.read()
+            
+        #converting frame(img i.e BGR) to HSV (hue-saturation-value)
+
+        hsv=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+
+
+        #finding the range of red,blue and yellow color in the image
+        blue=cv.inRange(hsv,lower_blue,upper_blue)
+        yellow=cv.inRange(hsv,lower_red, upper_red)
+
+        #Morphological transformation, Dilation  	
+        kernal = np.ones((5 ,5), "uint8")
+
+        blue=cv.dilate(blue,kernal)
+        res1=cv.bitwise_and(img, img, mask = blue)
+
+        yellow=cv.dilate(yellow,kernal)
+        res2=cv.bitwise_and(img, img, mask = yellow)    
+
+
+        #Tracking the Blue Color
+        (contours,hierarchy)=cv.findContours(blue,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        for pic, contour in enumerate(contours):
+            area = cv.contourArea(contour)
+            if(area>300):
+                x,y,w,h = cv.boundingRect(contour)	
+                img = cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+        #Tracking the yellow Color
+        (contours,hierarchy)=cv.findContours(yellow,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        for pic, contour in enumerate(contours):
+            area = cv.contourArea(contour)
+            if(area>300):
+                x,y,w,h = cv.boundingRect(contour)	
+                img = cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+                
+        #cv.imshow("Redcolour",red)
+        cv.imshow("Color Tracking",img)
+        #cv.imshow("red",res) 	
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv.destroyAllWindows()
+            break  
+        
        
        
 def find_color_code():
@@ -93,4 +145,5 @@ def find_color_code():
     print(x[0][0])
 
 if __name__ == "__main__":
-    video_hsv_center_circle()
+    detectMultipleColors()
+    
