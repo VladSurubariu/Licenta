27463331347381f4b809b6545a76310e119bd703
Vlad Surubariu <vladsurubariu@gmail.com>
2023-05-14 17:46:44 +0300

    integrated the python script in the android app

--- JavaAndroidApp/app/src/main/java/com/example/test/MainActivity.java
+++ JavaAndroidApp/app/src/main/java/com/example/test/MainActivity.java
@@ -1,47 +1,103 @@
 package com.example.test;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.icu.util.Output;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buton;
+    private Button button;
+    private ImageView source_image;
     private TextView textView;
+    private String imageString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buton = findViewById(R.id.button);
-        textView = findViewById(R.id.textview);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textview);
+        source_image = (ImageView)findViewById(R.id.imageView2);
 
-        if (!Python.isStarted()){
-            Python.start(new AndroidPlatform(this));
-        }
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA},
+                PackageManager.PERMISSION_GRANTED);
 
-        Python py = Python.getInstance();
-        PyObject pyObject = py.getModule("script");
-        PyObject obj = pyObject.callAttr("main");
 
-        buton.setOnClickListener(new View.OnClickListener() {
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(obj.toString());
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 101);
             }
         });
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode == 101)
+        {
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            imageString = getStringImage(bitmap);
 
+            if (!Python.isStarted()){
+                Python.start(new AndroidPlatform(this));
+            }
+            Python py = Python.getInstance();
+            PyObject pyObject = py.getModule("script");
+            PyObject obj = pyObject.callAttr("main", imageString);
 
+            String str = obj.toString();
+            byte arraydata[] = android.util.Base64.decode(str,Base64.DEFAULT);
+            Bitmap bmp = BitmapFactory.decodeByteArray(arraydata, 0, arraydata.length);
+
+            source_image.setImageBitmap(bmp);
+            //textView.setText(str);
+
+        }
+    }
+
+    public String getStringImage(Bitmap bitmap)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = android.util.Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
 }
--- JavaAndroidApp/app/src/main/python/script.py
+++ JavaAndroidApp/app/src/main/python/script.py
@@ -1,2 +1,20 @@
-def main():
-    return "Salut!"
+import numpy as np
+import cv2
+import base64
+import io
+import PIL.Image as Image
+
+def main(data):
+
+    decoded_data = base64.b64decode(data)
+    np_data = np.fromstring(decoded_data, np.uint8)
+    img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+    #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.circle(img, (10, 10), 5, (255,0,0),3)
+    pil_im = Image.fromarray(img_gray)
+
+    buff = io.BytesIO()
+    pil_im.save(buff, format="PNG")
+    img_str = base64.b64encode(buff.getvalue())
+    #return img_str
+    return "" + str(img_str, 'utf-8')
